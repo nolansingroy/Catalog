e394b018c491c6a0371c724390cfc2634678fd88
vagrant <vagrant@vagrant.vm>
2018-01-05 20:08:00 +0000

    heroku build log stated crash due to application .py print statements therefore I commented them out!

--- application.py
+++ application.py
@@ -108,7 +108,7 @@ def gconnect():
         response = \
             make_response(json.dumps("Token's client ID does not match app's."
                                      ), 401)
-        #print "Token's client ID does not match app's."
+        # print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -156,7 +156,7 @@ def gconnect():
               -webkit-border-radius: 150px;-moz-border-radius: 150px;">
               """
     flash('you are now logged in as %s' % login_session['username'])
-    print 'done!'
+    # print 'done!'
     return output
 
 
@@ -164,21 +164,21 @@ def gconnect():
 def gdisconnect():
     access_token = login_session.get('access_token')
     if access_token is None:
-        print 'Access Token is None'
+        # print 'Access Token is None'
         response = \
             make_response(json.dumps('Current user not connected.'),
                           401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print 'In gdisconnect access token is %s', access_token
-    print 'User name is: '
-    print login_session['username']
+    # print 'In gdisconnect access token is %s', access_token
+    # print 'User name is: '
+    # print login_session['username']
     url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' \
         % login_session['access_token']
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
-    print 'result is '
-    print result
+    # print 'result is '
+    # print result
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
@@ -497,7 +497,7 @@ def createUser(login_session):
     return user.id
 
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
 #    app.run(host='0.0.0.0', port=8000)
